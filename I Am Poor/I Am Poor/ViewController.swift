aac54475fab3126643b29d7fd837d19aa4627b30
pyj <wodus331@gmail.com>
2026-03-05 23:03:46 +0900

    chapter 3 - do it myself

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by 박재연 on 3/5/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
